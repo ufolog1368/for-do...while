302298aab3c58b557c78974bddeb67412e610720
ufolog1368 <ufolog1368@gmail.com>
2020-09-17 22:45:27 +0300

    create test bean

--- Car/src/test/java/by/jis4/komarov/bean/CarTest.java
+++ Car/src/test/java/by/jis4/komarov/bean/CarTest.java
@@ -8,13 +8,29 @@ public class CarTest {
 
     @Test
     public void testEquals() {
+        var car1 = new Car("Dodge", "Grey metallik", 120);
+        var car2 = new Car("Dodge", "Grey metallik", 120);
+
+        boolean expected = true;
+        boolean actual = car1.equals(car2);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testHashCode() {
+        var car = new Car("Dodge", "Grey metallik", 120);
+
+        long expected = 1879403290;
+        long actual = car.hashCode();
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testToString() {
+        var car = new Car("Dodge", "Grey metallik", 120);
+
+        String expected = "Car{model='Dodge', color='Grey metallik', maxSpeed=120, currentSpeed=0}";
+        String atual = car.toString();
+        assertEquals(expected, atual);
     }
 }
